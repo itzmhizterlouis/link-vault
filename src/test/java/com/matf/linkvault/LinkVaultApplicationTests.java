67edfe0ec07a34c93ab069f8abbd108ee2ef5a1c
Marvellous <itzmhizterlouis@gmail.com>
2024-07-20 22:00:32 +0100

    Project and github up set up

---
+++ src/test/java/com/matf/linkvault/LinkVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matf.linkvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
